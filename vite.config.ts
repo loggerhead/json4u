f30afc7195684d5253c6861b97daa36b1662d2fb
loggerhead <lloggerhead@gmail.com>
2022-02-03 17:07:47 +0800

    feat: 使用插件注入 GA

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,19 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import analyzer from "rollup-plugin-analyzer";
 import importToCDN from "vite-plugin-cdn-import";
+import ViteRadar from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     analyzer({ summaryOnly: true }),
+    ViteRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: "G-TLYE3CBLPW",
+      },
+    }),
     importToCDN({
       modules: [
         {
