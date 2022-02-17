815f01fdd266289d07d46e8055c2f732cdd752e4
loggerhead <lloggerhead@gmail.com>
2022-02-17 11:52:38 +0800

    feat: 更改 GA

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import analyzer from "rollup-plugin-analyzer";
-import ViteRadar from "vite-plugin-radar";
 
 export default defineConfig({
   plugins: [
     vue(),
     // analyzer({ summaryOnly: true }),
-    ViteRadar({
-      analytics: {
-        id: "G-TLYE3CBLPW",
-      },
-    }),
   ],
   ssgOptions: {
     script: "async",
