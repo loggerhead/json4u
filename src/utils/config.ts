983bb18e9f037c0097c58948b5be1a72fb11da42
loggerhead <lloggerhead@gmail.com>
2022-04-23 09:51:42 +0800

    feat: 提供选项关闭自动格式化

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,8 @@
 export class Config {
   // 左右两边的 editor 同步滚动
   syncScroll: boolean = true;
-  // 隐藏右边的 editor（常用 format 时不需要 diff，隐藏可以展示更多内容）
-  hiddenRightEditor: boolean = false;
+  // 展示右边的 editor。如果经常用 format 而不是 diff，隐藏右边 editor 可以展示更多内容
+  showRightEditor: boolean = true;
+  // 粘贴时自动格式化
+  autoFormat: boolean = true;
 }
