8768548bf9df5450e7dfe8d283974150764bfd43
loggerhead <lloggerhead@gmail.com>
2023-07-31 21:41:34 +0800

    fix: jsonc-parser import umd 而不是 esm 的问题

--- next.config.js
+++ next.config.js
@@ -16,77 +16,9 @@ const withMDX = require("@next/mdx")({
   },
 });
 
-//
+// 包大小分析
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const path = require("path");
-const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
-// https://juejin.cn/post/7091177467498463239#heading-4
-const monacoConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  webpack: (config, { isServer, webpack, dev }) => {
-    config.module.rules
-      .filter((rule) => rule.oneOf)
-      .forEach((rule) => {
-        rule.oneOf.forEach((r) => {
-          if (
-            r.issuer &&
-            r.issuer.and &&
-            r.issuer.and.length === 1 &&
-            r.issuer.and[0].source &&
-            r.issuer.and[0].source.replace(/\\/g, "") === path.resolve(process.cwd(), "src/pages/_app")
-          ) {
-            r.issuer.or = [...r.issuer.and, /[\\/]node_modules[\\/]monaco-editor[\\/]/];
-            delete r.issuer.and;
-          }
-        });
-      });
-
-    config.output.globalObject = "self";
-
-    if (!isServer) {
-      config.plugins.push(
-        new MonacoWebpackPlugin({
-          languages: ["json"],
-          features: [
-            "coreCommands",
-            "bracketMatching",
-            "browser",
-            "caretOperations",
-            "comment",
-            "contextmenu",
-            "cursorUndo",
-            "dropOrPasteInto",
-            "find",
-            "folding",
-            "gotoLine",
-            "inPlaceReplace",
-            "indentation",
-            "lineSelection",
-            "linesOperations",
-            "linkedEditing",
-            "links",
-            "longLinesHelper",
-            "multicursor",
-            "quickCommand",
-            "quickOutline",
-            "smartSelect",
-            "stickyScroll",
-            "unusualLineTerminators",
-            "wordHighlighter",
-            "wordOperations",
-            "wordPartOperations",
-          ],
-          filename: "static/[name].worker.js",
-        })
-      );
-    }
-    return config;
-  },
-};
-
-module.exports = { ...withBundleAnalyzer(withMDX(nextConfig)), ...monacoConfig };
+module.exports = withBundleAnalyzer(withMDX(nextConfig));
