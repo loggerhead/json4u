e92b78bbe9d1e6b5a57223596ce0c4118dc59915
loggerhead <lloggerhead@gmail.com>
2023-08-06 14:13:06 +0800

    fix: 数据比较的 bug

--- __tests__/diff.test.js
+++ __tests__/diff.test.js
@@ -90,6 +90,23 @@ describe("Comparer", () => {
 });
 
 describe("semanticCompare", () => {
+  test("guide example", () => {
+    expectEq(
+      `{ "int64": 12345678987654321, "key": "value", "array": [12345678987654321, 1, 2, 3]}`,
+      `{ "int64": 12345678987654320, "kee": "value", "array": [12345678987654320, 2, 3, 1]}`,
+      [
+        new diff.Diff(27, 1, diff.DEL, false, ["int64"]),
+        new diff.Diff(27, 1, diff.INS, false, ["int64"]),
+        new diff.Diff(33, 1, diff.DEL, false, ["key"]),
+        new diff.Diff(33, 1, diff.INS, false, ["kee"]),
+        new diff.Diff(72, 1, diff.DEL, false, ["array", 0]),
+        new diff.Diff(72, 1, diff.INS, false, ["array", 0]),
+        new diff.Diff(75, 1, diff.DEL, true, ["array", 1]),
+        new diff.Diff(81, 1, diff.INS, true, ["array", 3]),
+      ]
+    );
+  });
+
   test("char compare", () => {
     expectEq(
       `  "foo": "abc" }`,
@@ -242,34 +259,50 @@ describe("semanticCompare", () => {
     expectEq(
       `{"foo":[{  "OBJ_ID": "CN=Kate Smith,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com",  "userAccountControl": "512",  "objectGUID": "b3067a77-875b-4208-9ee3-39128adeb654",  "lastLogon": "0",  "sAMAccountName": "ksmith",  "userPrincipalName": "ksmith@cloudaddc.qalab.cam.novell.com",  "distinguishedName": "CN=Kate Smith,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com"},{  "OBJ_ID": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com",  "userAccountControl": "512",  "objectGUID": "c3f7dae9-9b4f-4d55-a1ec-bf9ef45061c3",  "lastLogon": "130766915788304915",  "sAMAccountName": "tswan",  "userPrincipalName": "tswan@cloudaddc.qalab.cam.novell.com",  "distinguishedName": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com"}]}`,
       `[{  "OBJ_ID": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com",  "userAccountControl": "512",  "objectGUID": "c3f7dae9-9b4f-4d55-a1ec-bf9ef45061c3",  "lastLogon": "130766915788304915",  "sAMAccountName": "tswan",  "userPrincipalName": "tswan@cloudaddc.qalab.cam.novell.com",  "distinguishedName": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com"}]`,
-      [new diff.Diff(0, 1020, diff.DEL, true, []), new diff.Diff(0, 475, diff.INS, true, [])]
+      [new diff.Diff(0, 818, diff.DEL, true, []), new diff.Diff(0, 415, diff.INS, true, [])]
     );
 
     expectEq(
       `{"Aidan Gillen": {"array": ["Game of Thron\\"es","The Wire"],"string": "some string","int": 2,"aboolean": true, "boolean": true, "null": null, "a_null": null, "another_null": "null check", "object": {"foo": "bar","object1": {"new prop1": "new prop value"},"object2": {"new prop1": "new prop value"},"object3": {"new prop1": "new prop value"},"object4": {"new prop1": "new prop value"}}},"Amy Ryan": {"one": "In Treatment","two": "The Wire"},"Annie Fitzgerald": ["Big Love","True Blood"],"Anwan Glover": ["Treme","The Wire"],"Alexander Skarsgard": ["Generation Kill","True Blood"], "Clarke Peters": null}`,
       `{"Aidan Gillen": {"array": ["Game of Thrones","The Wire"],"string": "some string","int": "2","otherint": 4, "aboolean": "true", "boolean": false, "null": null, "a_null":88, "another_null": null, "object": {"foo": "bar"}},"Amy Ryan": ["In Treatment","The Wire"],"Annie Fitzgerald": ["True Blood","Big Love","The Sopranos","Oz"],"Anwan Glover": ["Treme","The Wire"],"Alexander Skarsg?rd": ["Generation Kill","True Blood"],"Alice Farmer": ["The Corner","Oz","The Wire"]}`,
       [
-        new diff.Diff(81, 6, diff.DEL, false, ["files.exclude", "bas"]),
-        new diff.Diff(81, 5, diff.INS, false, ["files.exclude", "bas"]),
+        new diff.Diff(42, 2, diff.DEL, false, ["Aidan Gillen", "array", 0]),
+        new diff.Diff(89, 3, diff.INS, true, []),
+        new diff.Diff(91, 1, diff.DEL, true, []),
+        new diff.Diff(105, 4, diff.DEL, true, []),
+        new diff.Diff(105, 1, diff.INS, true, ["Aidan Gillen", "otherint"]),
+        new diff.Diff(120, 6, diff.INS, true, []),
+        new diff.Diff(122, 3, diff.DEL, false, ["Aidan Gillen", "boolean"]),
+        new diff.Diff(139, 4, diff.INS, false, ["Aidan Gillen", "boolean"]),
+        new diff.Diff(152, 4, diff.DEL, true, []),
+        new diff.Diff(169, 2, diff.INS, true, []),
+        new diff.Diff(174, 12, diff.DEL, true, []),
+        new diff.Diff(189, 4, diff.INS, true, []),
+        new diff.Diff(223, 31, diff.DEL, true, ["Aidan Gillen", "object", "object1"]),
+        new diff.Diff(233, 27, diff.INS, true, []),
+        new diff.Diff(266, 31, diff.DEL, true, ["Aidan Gillen", "object", "object2"]),
+        new diff.Diff(306, 14, diff.INS, true, ["Annie Fitzgerald", 2]),
+        new diff.Diff(309, 31, diff.DEL, true, ["Aidan Gillen", "object", "object3"]),
+        new diff.Diff(321, 4, diff.INS, true, ["Annie Fitzgerald", 3]),
+        new diff.Diff(352, 31, diff.DEL, true, ["Aidan Gillen", "object", "object4"]),
+        new diff.Diff(381, 1, diff.INS, false, ["Alexander Skarsg?rd"]),
+        new diff.Diff(398, 41, diff.DEL, true, []),
+        new diff.Diff(436, 30, diff.INS, true, ["Alice Farmer"]),
+        new diff.Diff(540, 1, diff.DEL, false, ["Alexander Skarsgard"]),
+        new diff.Diff(597, 4, diff.DEL, true, ["Clarke Peters"]),
       ]
     );
 
     expectEq(
       `[{  "OBJ_ID": "CN=Kate Smith,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com",  "userAccountControl": "512",  "objectGUID": "b3067a77-875b-4208-9ee3-39128adeb654",  "lastLogon": "0",  "sAMAccountName": "ksmith",  "userPrincipalName": "ksmith@cloudaddc.qalab.cam.novell.com",  "distinguishedName": "CN=Kate Smith,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com"},{  "OBJ_ID": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com",  "userAccountControl": "512",  "objectGUID": "c3f7dae9-9b4f-4d55-a1ec-bf9ef45061c3",  "lastLogon": "130766915788304915",  "sAMAccountName": "tswan",  "userPrincipalName": "tswan@cloudaddc.qalab.cam.novell.com",  "distinguishedName": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com"}]`,
       `{"foo":[{  "OBJ_ID": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com",  "userAccountControl": "512",  "objectGUID": "c3f7dae9-9b4f-4d55-a1ec-bf9ef45061c3",  "lastLogon": "130766915788304915",  "sAMAccountName": "tswan",  "userPrincipalName": "tswan@cloudaddc.qalab.cam.novell.com",  "distinguishedName": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com"}]}`,
-      [
-        new diff.Diff(81, 6, diff.DEL, false, ["files.exclude", "bas"]),
-        new diff.Diff(81, 5, diff.INS, false, ["files.exclude", "bas"]),
-      ]
+      [new diff.Diff(0, 810, diff.DEL, true, []), new diff.Diff(0, 423, diff.INS, true, [])]
     );
 
     expectEq(
       `[{  "OBJ_ID": "CN=Kate Smith,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com",  "userAccountControl": "512",  "objectGUID": "b3067a77-875b-4208-9ee3-39128adeb654",  "lastLogon": "0",  "sAMAccountName": "ksmith",  "userPrincipalName": "ksmith@cloudaddc.qalab.cam.novell.com",  "distinguishedName": "CN=Kate Smith,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com"},{  "OBJ_ID": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com",  "userAccountControl": "512",  "objectGUID": "c3f7dae9-9b4f-4d55-a1ec-bf9ef45061c3",  "lastLogon": "130766915788304915",  "sAMAccountName": "tswan",  "userPrincipalName": "tswan@cloudaddc.qalab.cam.novell.com",  "distinguishedName": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com"}]`,
       `[{  "OBJ_ID": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com",  "userAccountControl": "512",  "objectGUID": "c3f7dae9-9b4f-4d55-a1ec-bf9ef45061c3",  "lastLogon": "130766915788304915",  "sAMAccountName": "tswan",  "userPrincipalName": "tswan@cloudaddc.qalab.cam.novell.com",  "distinguishedName": "CN=Timothy Swan,OU=Users,OU=Willow,DC=cloudaddc,DC=qalab,DC=cam,DC=novell,DC=com"}]`,
-      [
-        new diff.Diff(81, 6, diff.DEL, false, ["files.exclude", "bas"]),
-        new diff.Diff(81, 5, diff.INS, false, ["files.exclude", "bas"]),
-      ]
+      [new diff.Diff(1, 394, diff.DEL, true, [0])]
     );
   });
 });
