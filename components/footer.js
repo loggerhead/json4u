a167fbb060936967d3e4e0f7d1b86018869ad0b3
loggerhead <lloggerhead@gmail.com>
2023-08-19 11:51:30 +0800

    feat: 将外链标记为 nofollow

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="my-2 text-center text-[12px]">
       <a href="/">Json For You · </a>
-      <a href="https://beian.miit.gov.cn" target="_blank">
+      <a href="https://beian.miit.gov.cn" target="_blank" rel="nofollow">
         粤ICP备16007488号 ·{" "}
       </a>
       <Link prefetch={false} href="/guide">
