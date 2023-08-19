2bd662336e38afee4186645bb0693f91dc830836
loggerhead <lloggerhead@gmail.com>
2023-08-19 15:48:40 +0800

    feat: 支持通过按键暂停同步滚动

--- components/statusbar.js
+++ components/statusbar.js
@@ -1,7 +1,8 @@
-export default function StatusBar({ children }) {
+export default function StatusBar({ leftText, rightText }) {
   return (
-    <div className="h-[22px] text-[12px] border-[0.5px] border-t-0 border-solid border-color bg-slate-100">
-      <div className="px-2.5 py-0.5 statusbar-color">{children}</div>
+    <div className="flex h-[22px] text-[12px] border-[0.5px] border-t-0 border-solid border-color bg-slate-100">
+      <div className="px-2.5 py-0.5 grow statusbar-color">{leftText}</div>
+      <div className="px-2.5 py-0.5">{rightText}</div>
     </div>
   );
 }
