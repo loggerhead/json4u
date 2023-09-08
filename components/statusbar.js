5ab33547a7c42d5956184305adcec9142e64b798
loggerhead <lloggerhead@gmail.com>
2023-09-08 19:53:42 +0800

    refactor: alert.js 挪到 statusbar.js

--- components/statusbar.js
+++ components/statusbar.js
@@ -1,6 +1,6 @@
 "use client";
 import { useRef, useMemo } from "react";
-import MyAlert from "./alert";
+import * as color from "../lib/color";
 
 export default function StatusBar({ texts }) {
   const cacheMap = useRef({});
@@ -37,7 +37,7 @@ export default function StatusBar({ texts }) {
 
       return (
         <div key={key} className={classes.join(" ")}>
-          <MyAlert msg={textsMap[key]}></MyAlert>
+          <BarStub msg={textsMap[key]}></BarStub>
         </div>
       );
     });
@@ -49,5 +49,58 @@ export default function StatusBar({ texts }) {
       <div className="grow"></div>
       {genAlerts(rightKeys)}
     </div>
+  );
+}
+
+function BarStub({ msg }) {
+  let node;
+
+  if (typeof window != "undefined" && typeof DOMParser != "undefined") {
+    const [pureMsg, colors] = color.matchColorTags(msg);
+
+    if (pureMsg && colors.length) {
+      node = new DOMParser().parseFromString(msg, "text/xml");
+      const err = node?.querySelector("parsererror");
+
+      // 如果解析失败
+      if (pureMsg && err) {
+        node = undefined;
+        console.error(`文本节点解析错误:\n\n${msg}\n\n`, err?.textContent);
+      }
+    }
+
+    if (node === undefined) {
+      node = document.createTextNode(pureMsg);
+    }
+  }
+
+  return <BarStubNode key={`00`} node={node} level={0}></BarStubNode>;
+}
+
+// 根据 dom 树生成 span 树
+function BarStubNode({ node, level }) {
+  if (!node || node.nodeType == Node.TEXT_NODE) {
+    const text = node?.textContent || "";
+    return <span key={`${level}`}>{text}</span>;
+  }
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType
+  if (node.nodeType === Node.DOCUMENT_NODE) {
+    node = node.childNodes[0];
+  }
+  const color = node.nodeName;
+
+  return (
+    <span key={`${level}0`} className={`alert-${color}`}>
+      {Array.from(node?.childNodes).map((node, i) => {
+        const key = `${level}${i + 1}`;
+
+        if (node.nodeType === Node.ELEMENT_NODE) {
+          return <BarStubNode key={key} node={node} level={level + 1}></BarStubNode>;
+        } else {
+          return <span key={key}>{node.textContent}</span>;
+        }
+      })}
+    </span>
   );
 }
