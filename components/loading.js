f45195dfcd7228e48720f9092098ed4537deda74
loggerhead <lloggerhead@gmail.com>
2023-09-16 10:33:56 +0800

    feat: 「加载中...」换成动画

--- components/loading.js
+++ components/loading.js
@@ -1,7 +1,7 @@
 export default function Loading({ height }) {
   return (
     <div style={{ height: height }} className="flex items-center justify-center	text-2xl">
-      加载中...
+      <div className="loader"></div>
     </div>
   );
 }
