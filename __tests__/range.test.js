cd96b664fd8dceb7825538163bc753b6e2474dce
loggerhead <lloggerhead@gmail.com>
2023-09-25 15:37:38 +0800

    feat: 支持文本比较时进行填充

--- __tests__/range.test.js
+++ __tests__/range.test.js
@@ -1,105 +1,157 @@
-import { Range, sortRanges, uniqRanges, mergeLineRanges, generateFillRanges } from "../lib/compare/range";
+import * as compare from "../lib/compare";
 
-describe("ranges util", () => {
-  test("sortRanges", () => {
-    const tt = [
-      [[], []],
-      [[new Range(3, 3)], [new Range(3, 3)]],
-      [
-        [new Range(3, 3), new Range(1, 1), new Range(2, 2)],
-        [new Range(1, 1), new Range(2, 2), new Range(3, 3)],
-      ],
+describe("generateFillRanges", () => {
+  test("object case 1", () => {
+    const hunkDiffs = [
+      {
+        "type": "delete",
+        "range": {
+          "startLineNumber": 4,
+          "endLineNumber": 4,
+        },
+      },
+      {
+        "type": "insert",
+        "range": {
+          "startLineNumber": 4,
+          "endLineNumber": 4,
+        },
+      },
+      {
+        "type": "delete",
+        "range": {
+          "startLineNumber": 8,
+          "endLineNumber": 10,
+        },
+      },
+      {
+        "type": "insert",
+        "range": {
+          "startLineNumber": 8,
+          "endLineNumber": 11,
+        },
+      },
+      {
+        "type": "delete",
+        "range": {
+          "startLineNumber": 12,
+          "endLineNumber": 13,
+        },
+      },
+      {
+        "type": "insert",
+        "range": {
+          "startLineNumber": 13,
+          "endLineNumber": 14,
+        },
+      },
+      {
+        "type": "delete",
+        "range": {
+          "startLineNumber": 15,
+          "endLineNumber": 27,
+        },
+      },
+      {
+        "type": "insert",
+        "range": {
+          "startLineNumber": 16,
+          "endLineNumber": 16,
+        },
+      },
+      {
+        "type": "delete",
+        "range": {
+          "startLineNumber": 30,
+          "endLineNumber": 33,
+        },
+      },
+      {
+        "type": "insert",
+        "range": {
+          "startLineNumber": 19,
+          "endLineNumber": 22,
+        },
+      },
+      {
+        "type": "insert",
+        "range": {
+          "startLineNumber": 24,
+          "endLineNumber": 24,
+        },
+      },
+      {
+        "type": "delete",
+        "range": {
+          "startLineNumber": 36,
+          "endLineNumber": 36,
+        },
+      },
+      {
+        "type": "insert",
+        "range": {
+          "startLineNumber": 26,
+          "endLineNumber": 27,
+        },
+      },
+      {
+        "type": "delete",
+        "range": {
+          "startLineNumber": 42,
+          "endLineNumber": 42,
+        },
+      },
+      {
+        "type": "insert",
+        "range": {
+          "startLineNumber": 33,
+          "endLineNumber": 33,
+        },
+      },
+      {
+        "type": "delete",
+        "range": {
+          "startLineNumber": 46,
+          "endLineNumber": 46,
+        },
+      },
+      {
+        "type": "insert",
+        "range": {
+          "startLineNumber": 37,
+          "endLineNumber": 41,
+        },
+      },
     ];
 
-    for (const [input, expected] of tt) {
-      expect(sortRanges(input)).toEqual(expected);
-    }
-  });
-
-  test("uniqRanges", () => {
-    const tt = [
-      [[], []],
-      [[new Range(3, 3)], [new Range(3, 3)]],
-      [
-        [new Range(3, 3), new Range(1, 1), new Range(3, 3)],
-        [new Range(3, 3), new Range(1, 1)],
-      ],
+    const leftFills = [
+      {
+        "startLineNumber": 11,
+        "endLineNumber": 11,
+      },
+      {
+        "startLineNumber": 35,
+        "endLineNumber": 35,
+      },
+      {
+        "startLineNumber": 37,
+        "endLineNumber": 37,
+      },
+      {
+        "startLineNumber": 47,
+        "endLineNumber": 50,
+      },
     ];
 
-    for (const [input, expected] of tt) {
-      expect(uniqRanges(input)).toEqual(expected);
-    }
-  });
-
-  test("mergeLineRanges", () => {
-    const tt = [
-      [[], []],
-      [[new Range(3, 3)], [new Range(3, 3)]],
-      [[new Range(1, 1), new Range(2, 2), new Range(3, 3)], [new Range(1, 3)]],
-      [[new Range(1, 2), new Range(3, 4)], [new Range(1, 4)]],
-      [[new Range(1, 3), new Range(3, 4)], [new Range(1, 4)]],
-      [[new Range(1, 3), new Range(2, 4)], [new Range(1, 4)]],
-      [[new Range(1, 2), new Range(3, 3), new Range(4, 5)], [new Range(1, 5)]],
-      [
-        [new Range(1, 2), new Range(4, 5)],
-        [new Range(1, 2), new Range(4, 5)],
-      ],
-      [
-        [new Range(1, 10), new Range(5, 25), new Range(40, 50)],
-        [new Range(1, 25), new Range(40, 50)],
-      ],
+    const rightFills = [
+      {
+        "startLineNumber": 17,
+        "endLineNumber": 28,
+      },
     ];
 
-    for (const [input, expected] of tt) {
-      expect(new mergeLineRanges(input)).toEqual(expected);
-    }
-  });
-
-  test("generateFillRanges", () => {
-    const tt = [
-      [[], [], [], []],
-      [[new Range(3, 3)], [], [new Range(3, 3)], []],
-      [[], [new Range(3, 3)], [], [new Range(3, 3)]],
-      // [   ]
-      //       [   ]
-      [[new Range(1, 3)], [new Range(5, 7)], [new Range(1, 3)], [new Range(5, 7)]],
-      [[new Range(5, 7)], [new Range(1, 3)], [new Range(5, 7)], [new Range(1, 3)]],
-      // [   ]
-      //   [   ]
-      [[new Range(1, 5)], [new Range(3, 7)], [new Range(1, 2)], [new Range(6, 7)]],
-      [[new Range(3, 7)], [new Range(1, 5)], [new Range(6, 7)], [new Range(1, 2)]],
-      //   [  ]
-      // [       ]
-      [[new Range(3, 5)], [new Range(1, 7)], [], [new Range(1, 2), new Range(6, 7)]],
-      [[new Range(1, 7)], [new Range(3, 5)], [new Range(1, 2), new Range(6, 7)], []],
-      //  [ ] [ ]
-      // [        ]
-      [[new Range(2, 3), new Range(5, 6)], [new Range(1, 7)], [], [new Range(1, 1), new Range(4, 4), new Range(7, 7)]],
-      [[new Range(1, 7)], [new Range(2, 3), new Range(5, 6)], [new Range(1, 1), new Range(4, 4), new Range(7, 7)], []],
-      //  [ ] [ ] [  ]
-      // [         ]
-      [
-        [new Range(2, 3), new Range(5, 6), new Range(8, 10)],
-        [new Range(1, 9)],
-        [new Range(10, 10)],
-        [new Range(1, 1), new Range(4, 4), new Range(7, 7)],
-      ],
-    ];
-
-    for (const [leftRanges, rightRanges, leftExtras, rightExtras] of tt) {
-      expect(generateFillRanges(leftRanges, rightRanges)).toEqual([leftExtras, rightExtras]);
-    }
-  });
-
-  test("generateFillRanges case 1", () => {
-    expect(
-      generateFillRanges(
-        [new Range(13, 24), new Range(26, 27), new Range(35, 36)],
-        [new Range(13, 14), new Range(24, 27), new Range(37, 41)]
-      )
-    ).toEqual([
-      [new Range(37, 38), new Range(46, 50)],
-      [new Range(15, 24), new Range(17, 17)],
-    ]);
+    const [leftGot, rightGot] = compare.generateFillRanges(hunkDiffs);
+    expect(leftGot).toMatchObject(leftFills);
+    expect(rightGot).toMatchObject(rightFills);
   });
 });
