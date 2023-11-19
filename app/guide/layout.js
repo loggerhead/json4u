aa50391537323422b802d347f293c4a8cd6a3bd7
loggerhead <lloggerhead@gmail.com>
2023-11-19 22:01:09 +0800

    fix: guide 页样式问题

--- app/guide/layout.js
+++ app/guide/layout.js
@@ -1,8 +1,9 @@
 import "github-markdown-css";
 
-export default async function MdxLayout({ children }) {
+export default async function MdxLayout({children}) {
   return (
-    <div className="markdown-body relative w-full bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 md:max-w-3xl md:mx-auto lg:max-w-4xl lg:pt-16 lg:pb-28">
+    <div
+      className="markdown-body relative w-full bg-white shadow-xl shadow-slate-700/10 ring-1 ring-gray-900/5 p-8 md:max-w-3xl md:mx-auto lg:max-w-4xl">
       <div className="mt-8 prose prose-slate mx-auto lg:prose-lg">{children}</div>
     </div>
   );
