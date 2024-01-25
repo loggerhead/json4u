2805403c3c688ffb846331551e9c30fd97e62bf3
loggerhead <lloggerhead@gmail.com>
2024-01-25 10:46:13 +0800

    fix: footer 样式问题

--- components/feedback.js
+++ components/feedback.js
@@ -1,11 +1,10 @@
 "use client";
 import * as sentry from "@sentry/react";
-import {IconBug} from '@arco-design/web-react/icon';
 
 export default function Feedback() {
   const client = sentry.getClient();
   const feedback = client?.getIntegration(sentry.Feedback);
   return <button id="feedback" onClick={() => feedback.openDialog()}>
-    <IconBug/> 反馈
+    反馈
   </button>;
 }
--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import Feedback from "./feedback";
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 my-2 left-1/2 -translate-x-1/2 text-center text-[12px]">
+    <div className="my-2 left-1/2 text-center text-[12px]">
       <a href="/">JSON For You · </a>
       <a href="https://beian.miit.gov.cn" target="_blank" rel="nofollow">
         粤ICP备16007488号 ·{" "}
