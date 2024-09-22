6680f5179b4a6569875de02f613848e926a126a8
loggerhead <lloggerhead@gmail.com>
2024-09-22 13:58:11 +0800

    feat: support display expired date

--- src/components/AccountPanel.tsx
+++ src/components/AccountPanel.tsx
@@ -5,6 +5,7 @@ import LogoutButton from "@/components/LogoutButton";
 import { Button } from "@/components/ui/button";
 import { MessageKey } from "@/global";
 import { isCN } from "@/lib/env";
+import { Order } from "@/lib/supabase/table.types";
 import { cn, dateToYYYYMMDD } from "@/lib/utils";
 import { useUserStore } from "@/stores/userStore";
 import { useTranslations } from "next-intl";
@@ -28,6 +29,8 @@ export default function AccountPanel() {
 
   const name = user.user_metadata?.name;
   const email = user.email;
+  const status = activeOrder?.status;
+  const plan = getPlan();
 
   return (
     <div className="flex flex-col text-left px-3 pt-4 pb-2">
@@ -45,14 +48,9 @@ export default function AccountPanel() {
       <div className="flex flex-col mb-1 px-3 py-1 gap-1 bg-muted rounded-md">
         <span className="flex gap-1">
           <Typography affects="muted">{t("plan_display_prefix")}</Typography>
-          <Typography className="font-bold">{t(getPlan() as MessageKey)}</Typography>
+          <Typography className="font-bold">{`${t(plan as MessageKey)} (${status})`}</Typography>
         </span>
-        {activeOrder?.renews_at && (
-          <span className="flex gap-1">
-            <Typography affects="muted">{t("renew_display_prefix")}</Typography>
-            <Typography>{dateToYYYYMMDD(activeOrder.renews_at)}</Typography>
-          </span>
-        )}
+        <PremiumStatus activeOrder={activeOrder} />
       </div>
       <Button
         className={cn("justify-start", isCN && "hidden")}
@@ -65,5 +63,29 @@ export default function AccountPanel() {
       </Button>
       <LogoutButton />
     </div>
+  );
+}
+
+interface PremiumStatusProps {
+  activeOrder: Order | null;
+}
+
+function PremiumStatus({ activeOrder }: PremiumStatusProps) {
+  const t = useTranslations("Pricing");
+  const status = activeOrder?.status;
+  const renewsAt = activeOrder?.renews_at;
+  const endsAt = activeOrder?.ends_at;
+  const text: MessageKey = status === "active" ? "renew_display_prefix" : "expired_display_prefix";
+  const date = status === "active" ? renewsAt : endsAt;
+
+  if (!activeOrder || !date) {
+    return null;
+  }
+
+  return (
+    <span className="flex gap-1">
+      <Typography affects="muted">{t(text)}</Typography>
+      <Typography>{dateToYYYYMMDD(date)}</Typography>
+    </span>
   );
 }
