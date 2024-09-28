34a935b8c964e0fe309cc990d9685d7e506f24a8
loggerhead <lloggerhead@gmail.com>
2024-09-28 08:25:20 +0800

    refactor: adjust the landing page UI

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,21 +8,21 @@ import HeroCarousel from "@/containers/landing/HeroCarousel";
 import { Pricing } from "@/containers/pricing";
 import { useTranslations } from "next-intl";
 
+// TODO: redesign
 export default function Index() {
   const t = useTranslations("Home");
 
   return (
     <div className="relative flex flex-col items-center w-full h-full pt-12 pb-36 md:px-32 px-4 gap-24 text-center">
-      {/* TODO: redesign */}
       {/* <HeroTitle /> */}
-      <div className="mt-2" />
+      <div className="flex items-center gap-12">
+        <Typography variant="h3">{t("landing_title")}</Typography>
+        <LinkButton className="h-[54px]" size="lg" variant="default" href="/editor">
+          {/* TODO: 改为 start free */}
+          <Typography variant="h5">{t("try_now")}</Typography>
+        </LinkButton>
+      </div>
       <HeroCarousel />
-
-      {/* TODO: redesign */}
-      <LinkButton className="h-[54px]" size="lg" variant="default" href="/editor">
-        {/* TODO: 改为 start free */}
-        <Typography variant="h5">{t("try_now")}</Typography>
-      </LinkButton>
       <Features />
       <Pricing />
       <FAQ />
