b2249fa592636aec955a2ac92ca5d49c7bd78cdb
loggerhead <lloggerhead@gmail.com>
2024-09-28 11:57:26 +0800

    fix: overflow border when string of root node is too long in graph mode

--- src/containers/editor/graph/RootNode.tsx
+++ src/containers/editor/graph/RootNode.tsx
@@ -1,4 +1,4 @@
-import { NodeWithData, genValueAttrs, globalStyle } from "@/lib/graph/layout";
+import { NodeWithData, genValueAttrs } from "@/lib/graph/layout";
 import { useTree } from "@/stores/treeStore";
 import { type NodeProps } from "@xyflow/react";
 import { SourceHandle } from "./Handle";
@@ -18,7 +18,7 @@ export default function RootNode({ data, ...props }: NodeProps<NodeWithData>) {
     <>
       <Toolbar node={node} visible={data.toolbarVisible} />
       <div className="graph-node" style={data.style}>
-        <div className="graph-kv" style={{ height: globalStyle.kvHeight }}>
+        <div className="graph-kv">
           <div className={className}>{text}</div>
           <SourceHandle node={node} />
         </div>
