67e662d63f5313a2ce86b2fc8a1f25c3fdb10c8f
loggerhead <lloggerhead@gmail.com>
2024-10-01 09:55:25 +0800

    feat: only display visable nodes in graph viewport

--- src/containers/editor/graph/useNodesAndEdges.ts
+++ src/containers/editor/graph/useNodesAndEdges.ts
@@ -2,10 +2,18 @@ import { useEffect, useState } from "react";
 import { ViewMode } from "@/lib/db/config";
 import { type EdgeWithData, type NodeWithData } from "@/lib/graph/layout";
 import { useStatusStore } from "@/stores/statusStore";
-import { useGraph, useTreeVersion } from "@/stores/treeStore";
+import { useGraph, useTreeStore, useTreeVersion } from "@/stores/treeStore";
 import { useUserStore } from "@/stores/userStore";
-import { OnEdgesChange, OnNodesChange, useEdgesState, useNodesState, XYPosition } from "@xyflow/react";
+import {
+  OnEdgesChange,
+  OnNodesChange,
+  useEdgesState,
+  useNodesState,
+  useOnViewportChange,
+  XYPosition,
+} from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
+import { useResizeObserver } from "usehooks-ts";
 
 export interface NodesAndEdges {
   nodes: NodeWithData[];
@@ -18,12 +26,14 @@ export interface NodesAndEdges {
   version?: number;
 }
 
-export default function useNodesAndEdges(): NodesAndEdges {
+export default function useNodesAndEdges(ref: React.RefObject<HTMLDivElement>): NodesAndEdges {
   const count = useUserStore((state) => state.count);
   const usable = useUserStore((state) => state.usable("graphModeView"));
   const viewMode = useStatusStore((state) => state.viewMode);
   const setShowPricingOverlay = useStatusStore((state) => state.setShowPricingOverlay);
   const treeVersion = useTreeVersion();
+  const setGraphSize = useTreeStore((state) => state.setGraphSize);
+  const setGraphViewport = useTreeStore((state) => state.setGraphViewport);
   const { nodes: nextNodes, edges: nextEdges, levelMeta } = useGraph();
 
   const [version, setVersion] = useState(0);
@@ -42,6 +52,15 @@ export default function useNodesAndEdges(): NodesAndEdges {
       }
     }
   }, [usable, viewMode, treeVersion, version, nextNodes, nextEdges]);
+
+  useResizeObserver({
+    ref,
+    onResize: ({ width, height }) => setGraphSize(width, height),
+  });
+
+  useOnViewportChange({
+    onEnd: (viewport) => setGraphViewport(viewport),
+  });
 
   return { nodes, edges, levelMeta, setNodes, setEdges, onNodesChange, onEdgesChange, version };
 }
--- src/stores/treeStore.ts
+++ src/stores/treeStore.ts
@@ -3,6 +3,7 @@ import type { Graph } from "@/lib/graph/layout";
 import { Tree } from "@/lib/parser";
 import type { KeyWithType } from "@/lib/table";
 import { type FunctionKeys } from "@/lib/utils";
+import { Viewport } from "@xyflow/react";
 import { create } from "zustand";
 import { useShallow } from "zustand/react/shallow";
 
@@ -37,6 +38,8 @@ export interface TreeState {
   setTooltip: (content: TooltipContent) => void;
   hideTooltip: () => void;
   setGraph: (graph?: Graph) => void;
+  setGraphSize: (width?: number, height?: number) => void;
+  setGraphViewport: (viewport: Viewport) => void;
 }
 
 const initialStates: Omit<TreeState, FunctionKeys<TreeState>> = {
@@ -67,6 +70,21 @@ export const useTreeStore = create<TreeState>()((set, get) => ({
 
   setGraph(graph?: Graph) {
     set({ graph: graph ?? initialStates.graph });
+  },
+
+  // 1. get viewport (origin x, y in left-top)
+  // 2. compute current bounds (real size) of viewport
+  // 3. compute nodes includes in the bounds
+  // 4. compute edges includes in the bounds
+  // x/zoom, y/zoom 是实际大小
+  setGraphSize(width?: number, height?: number) {
+    const { graph } = get();
+    set({ graph: { ...graph, width, height } });
+  },
+
+  setGraphViewport(viewport: Viewport) {
+    const { graph } = get();
+    set({ graph: { ...graph, viewport } });
   },
 }));
 
