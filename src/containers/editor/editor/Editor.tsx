462c35cad2e6e4540d372f0524bfb04341ee0cea
loggerhead <lloggerhead@gmail.com>
2024-10-07 10:49:40 +0800

    feat: enable sticky scroll

--- src/containers/editor/editor/Editor.tsx
+++ src/containers/editor/editor/Editor.tsx
@@ -39,6 +39,10 @@ function MyEditor({ kind, ...props }: EditorProps) {
         automaticLayout: true, // 当编辑器所在的父容器的大小改变时，编辑器会自动重新计算并调整大小
         wordWrap: "off", // 关闭软换行。避免软换行时，点击出现不规则滚动的问题
         minimap: { enabled: false },
+        stickyScroll: {
+          enabled: true,
+          defaultModel: "foldingProviderModel",
+        },
       }}
       onMount={(editor) => {
         const wrapper = new EditorWrapper(editor, kind);
