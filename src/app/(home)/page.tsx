7e87a396a563cf58c67701051364692f642845f4
loggerhead <lloggerhead@gmail.com>
2024-10-11 20:38:58 +0800

    fix: maximum call stack size exceeded when using the dots operator when generate table

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,11 +2,8 @@ import FAQ from "@/containers/landing/FAQ";
 import Features from "@/containers/landing/Features";
 import HeroTitle from "@/containers/landing/HeroTitle";
 import { Pricing } from "@/containers/pricing";
-import { useTranslations } from "next-intl";
 
 export default function Index() {
-  const t = useTranslations("Home");
-
   return (
     <div className="relative flex flex-col items-center w-full h-full md:mx-32 mx-4 mb-48 space-y-24 text-center">
       <HeroTitle />
