f9ba92f6be1484118da46e2a14c15a3024000e82
loggerhead <lloggerhead@gmail.com>
2024-11-03 16:22:17 +0800

    feat: open wordWrap on editor

--- src/containers/editor/editor/Editor.tsx
+++ src/containers/editor/editor/Editor.tsx
@@ -37,7 +37,7 @@ function MyEditor({ kind, ...props }: EditorProps) {
         fontSize: 13, // 设置初始字体大小
         scrollBeyondLastLine: false, // 行数超过一屏时才展示滚动条
         automaticLayout: true, // 当编辑器所在的父容器的大小改变时，编辑器会自动重新计算并调整大小
-        wordWrap: "off", // 关闭软换行。避免软换行时，点击出现不规则滚动的问题
+        wordWrap: "on",
         minimap: { enabled: false },
         stickyScroll: {
           enabled: true,
