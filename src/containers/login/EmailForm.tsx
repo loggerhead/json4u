fbb0e7c04528e18236967b6476eef0dcfea6898f
loggerhead <lloggerhead@gmail.com>
2024-12-05 07:26:03 +0800

    refactor: remove captcha when login via email

--- src/containers/login/EmailForm.tsx
+++ src/containers/login/EmailForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { RefObject, useRef, useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import LoadingButton from "@/components/LoadingButton";
 import { Button } from "@/components/ui/button";
@@ -8,10 +8,8 @@ import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "
 import { Input } from "@/components/ui/input";
 import { InputOTP, InputOTPGroup, InputOTPSlot } from "@/components/ui/input-otp";
 import Typography from "@/components/ui/typography";
-import { env } from "@/lib/env";
 import { supabase } from "@/lib/supabase/client";
 import { toastErr, toastSucc } from "@/lib/utils";
-import HCaptcha from "@hcaptcha/react-hcaptcha";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { sendGAEvent } from "@next/third-parties/google";
 import { useTranslations } from "next-intl";
@@ -30,85 +28,73 @@ type FormData = z.infer<typeof FormSchema>;
 
 export default function EmailForm() {
   const t = useTranslations("Home");
-  const captchaRef = useRef<HCaptcha>(null);
   const form = useForm<FormData>({
     resolver: zodResolver(FormSchema),
   });
   const [email] = form.watch(["email"]);
-  const { pending, isCounting, countdown, sendOTP } = useSendOTP(captchaRef, email);
+  const { pending, isCounting, countdown, sendOTP } = useSendOTP(email);
   const { loading, onVerifyOTP } = useVerifyOTP();
 
   return (
-    <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onVerifyOTP)} className="w-full">
-          <div className="flex flex-col gap-4">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t("email_label")}</FormLabel>
-                  <FormControl>
-                    <Input className="py-5" disabled={loading} placeholder={t("email_placeholder")} {...field} />
-                  </FormControl>
-                  <FormMessage errorMessage={t("invalid_email_error")} />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="otp"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t("otp_label")}</FormLabel>
-                  <FormControl>
-                    <InputOTP maxLength={6} {...field}>
-                      <InputOTPGroup>
-                        <InputOTPSlot index={0} />
-                        <InputOTPSlot index={1} />
-                        <InputOTPSlot index={2} />
-                        <InputOTPSlot index={3} />
-                        <InputOTPSlot index={4} />
-                        <InputOTPSlot index={5} />
-                      </InputOTPGroup>
-                    </InputOTP>
-                  </FormControl>
-                  <FormMessage errorMessage={t("invalid_otp_error")}>{t("email_otp_placeholder")}</FormMessage>
-                </FormItem>
-              )}
-            />
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onVerifyOTP)} className="w-full">
+        <div className="flex flex-col gap-4">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{t("email_label")}</FormLabel>
+                <FormControl>
+                  <Input className="py-5" disabled={loading} placeholder={t("email_placeholder")} {...field} />
+                </FormControl>
+                <FormMessage errorMessage={t("invalid_email_error")} />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="otp"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>{t("otp_label")}</FormLabel>
+                <FormControl>
+                  <InputOTP maxLength={6} {...field}>
+                    <InputOTPGroup>
+                      <InputOTPSlot index={0} />
+                      <InputOTPSlot index={1} />
+                      <InputOTPSlot index={2} />
+                      <InputOTPSlot index={3} />
+                      <InputOTPSlot index={4} />
+                      <InputOTPSlot index={5} />
+                    </InputOTPGroup>
+                  </InputOTP>
+                </FormControl>
+                <FormMessage errorMessage={t("invalid_otp_error")}>{t("email_otp_placeholder")}</FormMessage>
+              </FormItem>
+            )}
+          />
 
-            <div className="flex w-full gap-3 mt-2">
-              <Button
-                className="flex-1 py-5 gap-1"
-                variant="default"
-                disabled={pending || isCounting}
-                onClick={async (ev) => {
-                  ev.preventDefault();
-                  await sendOTP();
-                }}
-              >
-                <Typography>{t("send_email_otp")}</Typography>
-                {isCounting && <Typography className="mono-font">{`(${countdown}s)`}</Typography>}
-              </Button>
-              <LoadingButton className="flex-1 py-5" type="submit" variant="default" loading={loading}>
-                <Typography>{t("login")}</Typography>
-              </LoadingButton>
-            </div>
+          <div className="flex w-full gap-3 mt-2">
+            <Button
+              className="flex-1 py-5 gap-1"
+              variant="default"
+              disabled={pending || isCounting}
+              onClick={async (ev) => {
+                ev.preventDefault();
+                await sendOTP();
+              }}
+            >
+              <Typography>{t("send_email_otp")}</Typography>
+              {isCounting && <Typography className="mono-font">{`(${countdown}s)`}</Typography>}
+            </Button>
+            <LoadingButton className="flex-1 py-5" type="submit" variant="default" loading={loading}>
+              <Typography>{t("login")}</Typography>
+            </LoadingButton>
           </div>
-        </form>
-      </Form>
-      <HCaptcha
-        ref={captchaRef}
-        sitekey={env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY}
-        size="invisible"
-        onError={(error) => {
-          sendGAEvent("event", "captcha", { error });
-          toastErr(t("captcha_error", { message: error }));
-        }}
-      />
-    </>
+        </div>
+      </form>
+    </Form>
   );
 }
 
@@ -146,7 +132,7 @@ function useVerifyOTP() {
   return { loading, onVerifyOTP };
 }
 
-function useSendOTP(captchaRef: RefObject<HCaptcha>, email: string) {
+function useSendOTP(email: string) {
   const t = useTranslations("Home");
   const [pending, setPending] = useState(false);
   const [count, { startCountdown, resetCountdown }] = useCountdown({
@@ -155,16 +141,8 @@ function useSendOTP(captchaRef: RefObject<HCaptcha>, email: string) {
   });
 
   const sendOTP = async () => {
-    const res = await captchaRef.current?.execute({ async: true });
-    const captchaToken = res?.response ?? "";
-
     setPending(true);
-    captchaRef.current?.resetCaptcha();
-
-    const { error } = await supabase.auth.signInWithOtp({
-      email,
-      options: { captchaToken },
-    });
+    const { error } = await supabase.auth.signInWithOtp({ email });
 
     sendGAEvent("event", "send_otp", {
       error: error?.message ?? "succ",
