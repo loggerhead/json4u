8aa0a2f75aa27f1cf5899aabbda42638e8e1b6b8
loggerhead <lloggerhead@gmail.com>
2025-08-12 07:10:05 +0800

    feat: upgrade monaco editor to 0.52.2

--- src/lib/editor/cdn.ts
+++ src/lib/editor/cdn.ts
@@ -1,6 +1,6 @@
 import { isCN } from "@/lib/env";
 
-const version = "0.52.0";
+const version = "0.52.2";
 const cndHost = isCN ? "o.json4u.cn" : "cdnjs.cloudflare.com";
 export const vsURL = `https://${cndHost}/ajax/libs/monaco-editor/${version}/min/vs`;
 export const loaderURL = `${vsURL}/loader.js`;
